65ae034a439cec695885a3d0d4be5225d45d9a68
DVijayan110284 <D_Vijayan@Dell.com>
2019-09-04 17:36:34 +0530

    Initial commit

--- src/PalTracker/TimeEntryController.cs
+++ src/PalTracker/TimeEntryController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace PalTracker
 {
-    [Route(" ")]
+    [Route("/time-entries")]
     public class TimeEntryController : ControllerBase
     {
         private ITimeEntryRepository _iTimeEntryRepository;
