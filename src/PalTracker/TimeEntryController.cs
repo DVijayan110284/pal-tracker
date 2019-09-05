f106669787f2d182b827054d3bdb6024b741b369
DVijayan110284 <D_Vijayan@Dell.com>
2019-09-05 14:56:58 +0530

    Initial commit

--- src/PalTracker/TimeEntryController.cs
+++ src/PalTracker/TimeEntryController.cs
@@ -6,14 +6,18 @@ namespace PalTracker
     public class TimeEntryController : ControllerBase
     {
         private ITimeEntryRepository _iTimeEntryRepository;
-        public TimeEntryController(ITimeEntryRepository iTimeEntryRepository)
+        private readonly IOperationCounter<TimeEntry> _operationCounter;
+
+        public TimeEntryController(ITimeEntryRepository iTimeEntryRepository, IOperationCounter<TimeEntry> operationCounter)
         {
             _iTimeEntryRepository = iTimeEntryRepository;
+            _operationCounter = operationCounter;
         }
 
         [HttpPost]
         public IActionResult Create([FromBody] TimeEntry timeEntry)
         {
+           _operationCounter.Increment(TrackedOperation.Create);
             var createdTimeEntry = _iTimeEntryRepository.Create(timeEntry);
 
             return CreatedAtRoute("GetTimeEntry", new {id = createdTimeEntry.Id}, createdTimeEntry);
@@ -22,24 +26,28 @@ namespace PalTracker
         [HttpGet("{id}", Name = "GetTimeEntry")]
         public IActionResult Read(long id)
         {
+             _operationCounter.Increment(TrackedOperation.Read);
             return _iTimeEntryRepository.Contains(id) ? (IActionResult) Ok(_iTimeEntryRepository.Find(id)) : NotFound();
         }
 
         [HttpGet]
         public IActionResult List()
         {
+            _operationCounter.Increment(TrackedOperation.Read);
             return Ok(_iTimeEntryRepository.List());
         }
 
         [HttpPut("{id}")]
         public IActionResult Update(long id, [FromBody] TimeEntry timeEntry)
         {
+             _operationCounter.Increment(TrackedOperation.Read);
             return _iTimeEntryRepository.Contains(id) ? (IActionResult) Ok(_iTimeEntryRepository.Update(id, timeEntry)) : NotFound();
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(long id)
         {
+             _operationCounter.Increment(TrackedOperation.Read);
             if (!_iTimeEntryRepository.Contains(id))
             {
                 return NotFound();
